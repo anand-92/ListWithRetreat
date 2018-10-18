f4380c9bb112b65df6bf87214ac16b6058428a91
harledge <42972091+harledge@users.noreply.github.com>
2018-10-18 12:20:03 -0400

    Update ListTest.java

--- ListTest.java
+++ ListTest.java
@@ -7,7 +7,7 @@ import components.list.List;
 /**
  * JUnit test fixture for {@code List<String>}'s constructor and kernel methods.
  *
- * @author Put your name here
+ * @author Nik Anand and Hudson Arledge
  *
  */
 public abstract class ListTest {
